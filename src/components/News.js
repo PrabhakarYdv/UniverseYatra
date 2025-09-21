cc7af358980c14e1a594e625813c01f5504c431f
PrabhakarYdv <prabhakarkr802@gmail.com>
2025-09-22 03:41:07 +0530

    News Container

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 import NewsCard from './NewsCard';
 
 export class News extends Component {
-  render() {
-    return (
-      <>
-        <NewsCard/>
-      </>
-    )
-  }
+    render() {
+        return (
+            <>
+                <div className="news-container">
+                    <NewsCard />
+                    <NewsCard />
+                    <NewsCard />
+                    <NewsCard />
+                    <NewsCard />
+                    <NewsCard />
+                </div>
+
+            </>
+        )
+    }
 }
 
 export default News;
