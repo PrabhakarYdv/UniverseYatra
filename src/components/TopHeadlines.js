d8767ea6c9f0c76be4450b0763f9bd92c7f39eb7
PrabhakarYdv <prabhakarkr802@gmail.com>
2025-09-23 19:16:16 +0530

    Top Headline

--- src/components/TopHeadlines.js
+++ src/components/TopHeadlines.js
@@ -4,7 +4,64 @@ export class TopHeadlines extends Component {
     render() {
         return (
             <>
-            <h5 className='top-headline-heading'>Top Headlines</h5>
+                <h5 className='top-headline-heading'>Top Headlines</h5>
+                <div className="headline-container">
+                    <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, laudantium.</p>
+                        </div>
+                    </div>
+
+
+                    <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div> <div className="headline-card">
+                        <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?" alt="poster" />
+                        <div className="headline-title">
+                            <p>This is Headline</p>
+                        </div>
+                    </div>
+
+                </div>
             </>
         )
     }
