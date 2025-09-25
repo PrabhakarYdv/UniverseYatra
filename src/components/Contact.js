867527bd167f0a26b2cf95156ff4e188b208c274
PrabhakarYdv <prabhakarkr802@gmail.com>
2025-09-25 15:13:43 +0530

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
-
 export class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      backgroundColor: "#FFFFFF"
+    }
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.randomColorGenerator, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+
+  randomColorGenerator = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    const randomHexCode = "#" + randomNumber.toString(16)
+    this.setState({ backgroundColor: randomHexCode })
+  }
+
   render() {
+
+    const { backgroundColor } = this.state
+    const style = {
+      backgroundColor: backgroundColor
+    }
     return (
-      <div>Contact</div>
+      <>
+        <div className="contact-container" style={style}>
+          <div className="contact-box">
+            <h2>UniverseYatra - Contact Us</h2>
+            <div className="query-box">
+              <form action="#">
+                <input type="text" placeholder='Enter Your Name . . .' required />
+                <input type="email" placeholder='Enter Your Email . . .' required />
+                <textarea placeholder='Enter Your Message . . .' rows="10" cols="50" required></textarea>
+                <button>Send Message</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
     )
   }
 }
 
-export default Contact
+export default Contact;
