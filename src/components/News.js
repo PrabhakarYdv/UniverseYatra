0b78ab232a636e167c91a789a0913adc08e0eb04
PrabhakarYdv <prabhakarkr802@gmail.com>
2025-10-14 23:16:42 +0530

    Add Previous and Next Function

--- src/components/News.js
+++ src/components/News.js
@@ -10,7 +10,10 @@ export class News extends Component {
         this.state = {
             articles: [],
             topHeadlines: [],
-            loading: false
+            loading: false,
+            page: 1,
+            totalData: 0,
+            isDataEnd: false
         }
     }
 
@@ -18,7 +21,7 @@ export class News extends Component {
 
     async componentDidMount() {
         const { apiKey } = this
-        let allNewsUrl = `https://newsapi.org/v2/everything?q=all&apiKey=${apiKey}&pageSize=18`
+        let allNewsUrl = `https://newsapi.org/v2/everything?q=all&apiKey=${apiKey}&pageSize=18&page=${this.state.page}`
         let topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?q=news&apiKey=${apiKey}`
         this.setState({ loading: true })
         let data = await fetch(allNewsUrl)
@@ -32,25 +35,50 @@ export class News extends Component {
         // console.log(this.state.topHeadlines)
     }
 
+    handlePrevBtn = async () => {
+        const { apiKey } = this
+        let allNewsUrl = `https://newsapi.org/v2/everything?q=all&apiKey=${apiKey}&pageSize=18&page=${this.state.page - 1}`
+        window.scrollTo(0, 0)
+        this.setState({ loading: true })
+        let data = await fetch(allNewsUrl)
+        let allNews = await data.json()
+        this.setState({ articles: allNews.articles || [], loading: false, page: this.state.page - 1 })
+    }
+
+    handleNextBtn = async () => {
+        const { apiKey } = this
+        let allNewsUrl = `https://newsapi.org/v2/everything?q=all&apiKey=${apiKey}&pageSize=18&page=${this.state.page + 1}`
+        window.scrollTo(0, 0)
+        this.setState({ loading: true })
+        let data = await fetch(allNewsUrl)
+        let allNews = await data.json()
+        this.setState({ articles: allNews.articles || [], loading: false, page: this.state.page + 1 })
+    }
+
     render() {
         return (
             <>
-                <div className="news-container">
-                    {this.state.loading && <Loading />}
-                    <div className="news-box">
-                        {!this.state.loading && this.state.articles.map((element) => {
-                            let publishDate = element.publishedAt.split("T")[0]
-                            return <NewsCard key={element.url} imageUrl={element.urlToImage} title={element.title} description={element.description} date={publishDate} author={element.author} url={element.url} />
-                        })}
+                <div className="main">
+                    <div className="news-container">
+                        {this.state.loading && <Loading />}
+                        <div className="news-box">
+                            {!this.state.loading && this.state.articles.map((element) => {
+                                let publishDate = element.publishedAt.split("T")[0]
+                                return <NewsCard key={element.url} imageUrl={element.urlToImage} title={element.title} description={element.description} date={publishDate} author={element.author} url={element.url} />
+                            })}
+                        </div>
+                        <div className="top-headlines">
+                            <h5 className='top-headline-heading'>Top Headlines</h5>
+                            {this.state.topHeadlines.map((element) => {
+                                return <TopHeadlines key={element.url} url={element.url} headlineImageUrl={element.urlToImage} headlineTitle={element.description} />
+                            })}
+                        </div>
                     </div>
-                    <div className="top-headlines">
-                        <h5 className='top-headline-heading'>Top Headlines</h5>
-                        {this.state.topHeadlines.map((element) => {
-                            return <TopHeadlines key={element.url} url={element.url} headlineImageUrl={element.urlToImage} headlineTitle={element.description} />
-                        })}
-                    </div>
+                    {!this.state.loading && <div className="prev-next">
+                        <button disabled={this.state.page <= 1} onClick={this.handlePrevBtn} className='prevBtn'>{"<<"}</button>
+                        <button disabled={this.state.isDataEnd} onClick={this.handleNextBtn} className='nextBtn'>{">>"}</button>
+                    </div>}
                 </div>
-
             </>
         )
     }
